bef91cf2e42a48430663699e562cb74d5cd6e603
Ryan Moylan <contact@ryanmoylan.com>
2023-07-26 13:23:46 -0400

    add create list item comment

--- backend/graphql/types/ListItemComment.ts
+++ backend/graphql/types/ListItemComment.ts
@@ -1,6 +1,17 @@
 import { builder } from "../builder";
 import prisma from "../../lib/prisma";
 
+const CreateListItemCommentInput = builder.inputType(
+  "CreateListItemCommentInput",
+  {
+    fields: (t) => ({
+      message: t.string({ required: true }),
+      authorId: t.string({ required: true }),
+      listItemId: t.string({ required: true }),
+    }),
+  }
+);
+
 builder.prismaObject("ListItemComment", {
   fields: (t) => ({
     id: t.exposeID("id"),
@@ -21,5 +32,28 @@ builder.queryField("listItemComments", (t) =>
     type: ["ListItemComment"],
     resolve: async (query, root, args, ctx, info) =>
       prisma.listItemComment.findMany({ ...query }),
+  })
+);
+
+builder.mutationField("createListItemComment", (t) =>
+  t.prismaField({
+    type: "ListItemComment",
+    nullable: true,
+    args: {
+      input: t.arg({ type: CreateListItemCommentInput, required: true }),
+    },
+    errors: {
+      types: [Error],
+    },
+    resolve: async (query, _, { input }) => {
+      return prisma.listItemComment.create({
+        ...query,
+        data: {
+          message: input.message,
+          authorId: input.authorId,
+          listItemId: input.listItemId,
+        },
+      });
+    },
   })
 );
